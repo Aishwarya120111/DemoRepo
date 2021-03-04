a5cd26b1d58c0ddbe05721d8596529a535529156
Aishwarya120111 <nadimpallyaishwarya@outlook.com>
2021-03-04 15:20:50 +0530

    New branch code

--- Demo.py
+++ Demo.py
@@ -3,6 +3,8 @@ num2 = 5
 
 # Adding two nos
 sum = num1 + num2
+product = num1 * num2
 
 # printing values
 print("Sum of {0} and {1} is {2}".format(num1, num2, sum))
+print("Product of {0} and {1} is {2}".format(num1, num2, product))
